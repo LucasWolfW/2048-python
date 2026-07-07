d4c7e1e19bc0b84659b9561d8403f3905d5ded50
Lucas Wolf <lucasgiovanew@gmail.com>
2026-07-07 00:09:35 -0300

    Refatoração

--- logic.py
+++ logic.py
@@ -1,22 +1,5 @@
-#
-# CS1010FC --- Programming Methodology
-#
-# Mission N Solutions
-#
-# Note that written answers are commented out to allow us to run your
-# code easily while grading your problem set.
-
 import random
 import constants as c
-
-#######
-# Task 1a #
-#######
-
-# [Marking Scheme]
-# Points to note:
-# Matrix elements must be equal but not identical
-# 1 mark for creating the correct matrix
 
 def new_game(n):
     matrix = []
@@ -25,15 +8,6 @@ def new_game(n):
     matrix = add_two(matrix)
     matrix = add_two(matrix)
     return matrix
-
-###########
-# Task 1b #
-###########
-
-# [Marking Scheme]
-# Points to note:
-# Must ensure that it is created on a zero entry
-# 1 mark for creating the correct loop
 
 def add_two(mat):
     a = random.randint(0, len(mat)-1)
@@ -44,53 +18,36 @@ def add_two(mat):
     mat[a][b] = 2
     return mat
 
-###########
-# Task 1c #
-###########
-
-# [Marking Scheme]
-# Points to note:
-# Matrix elements must be equal but not identical
-# 0 marks for completely wrong solutions
-# 1 mark for getting only one condition correct
-# 2 marks for getting two of the three conditions
-# 3 marks for correct checking
 
 def game_state(mat):
+
     # check for win cell
     for i in range(len(mat)):
         for j in range(len(mat[0])):
             if mat[i][j] == 2048:
                 return 'win'
+            
     # check for any zero entries
     for i in range(len(mat)):
         for j in range(len(mat[0])):
             if mat[i][j] == 0:
                 return 'not over'
+            
     # check for same cells that touch each other
     for i in range(len(mat)-1):
-        # intentionally reduced to check the row on the right and below
-        # more elegant to use exceptions but most likely this will be their solution
         for j in range(len(mat[0])-1):
             if mat[i][j] == mat[i+1][j] or mat[i][j+1] == mat[i][j]:
                 return 'not over'
-    for k in range(len(mat)-1):  # to check the left/right entries on the last row
+            
+    for k in range(len(mat)-1):
         if mat[len(mat)-1][k] == mat[len(mat)-1][k+1]:
             return 'not over'
-    for j in range(len(mat)-1):  # check up/down entries on last column
+        
+    for j in range(len(mat)-1):
         if mat[j][len(mat)-1] == mat[j+1][len(mat)-1]:
             return 'not over'
+        
     return 'lose'
-
-###########
-# Task 2a #
-###########
-
-# [Marking Scheme]
-# Points to note:
-# 0 marks for completely incorrect solutions
-# 1 mark for solutions that show general understanding
-# 2 marks for correct solutions that work for all sizes of matrices
 
 def reverse(mat):
     new = []
@@ -100,16 +57,6 @@ def reverse(mat):
             new[i].append(mat[i][len(mat[0])-j-1])
     return new
 
-###########
-# Task 2b #
-###########
-
-# [Marking Scheme]
-# Points to note:
-# 0 marks for completely incorrect solutions
-# 1 mark for solutions that show general understanding
-# 2 marks for correct solutions that work for all sizes of matrices
-
 def transpose(mat):
     new = []
     for i in range(len(mat[0])):
@@ -118,30 +65,17 @@ def transpose(mat):
             new[i].append(mat[j][i])
     return new
 
-##########
-# Task 3 #
-##########
-
-# [Marking Scheme]
-# Points to note:
-# The way to do movement is compress -> merge -> compress again
-# Basically if they can solve one side, and use transpose and reverse correctly they should
-# be able to solve the entire thing just by flipping the matrix around
-# No idea how to grade this one at the moment. I have it pegged to 8 (which gives you like,
-# 2 per up/down/left/right?) But if you get one correct likely to get all correct so...
-# Check the down one. Reverse/transpose if ordered wrongly will give you wrong result.
-
-def cover_up(mat):
+def cover_up(mat, grid_len):
     new = []
-    for j in range(c.GRID_LEN):
+    for j in range(grid_len):
         partial_new = []
-        for i in range(c.GRID_LEN):
+        for i in range(grid_len):
             partial_new.append(0)
         new.append(partial_new)
     done = False
-    for i in range(c.GRID_LEN):
+    for i in range(grid_len):
         count = 0
-        for j in range(c.GRID_LEN):
+        for j in range(grid_len):
             if mat[i][j] != 0:
                 new[i][count] = mat[i][j]
                 if j != count:
@@ -149,9 +83,9 @@ def cover_up(mat):
                 count += 1
     return new, done
 
-def merge(mat, done):
-    for i in range(c.GRID_LEN):
-        for j in range(c.GRID_LEN-1):
+def merge(mat, done, grid_len):
+    for i in range(grid_len):
+        for j in range(grid_len-1):
             if mat[i][j] == mat[i][j+1] and mat[i][j] != 0:
                 mat[i][j] *= 2
                 mat[i][j+1] = 0
@@ -160,38 +94,38 @@ def merge(mat, done):
 
 def up(game):
     print("up")
-    # return matrix after shifting up
+    grid_len = len(game)
     game = transpose(game)
-    game, done = cover_up(game)
-    game, done = merge(game, done)
-    game = cover_up(game)[0]
+    game, done = cover_up(game, grid_len)
+    game, done = merge(game, done, grid_len)
+    game = cover_up(game, grid_len)[0]
     game = transpose(game)
     return game, done
 
 def down(game):
     print("down")
-    # return matrix after shifting down
+    grid_len = len(game)
     game = reverse(transpose(game))
-    game, done = cover_up(game)
-    game, done = merge(game, done)
-    game = cover_up(game)[0]
+    game, done = cover_up(game, grid_len)
+    game, done = merge(game, done, grid_len)
+    game = cover_up(game, grid_len)[0]
     game = transpose(reverse(game))
     return game, done
 
 def left(game):
     print("left")
-    # return matrix after shifting left
-    game, done = cover_up(game)
-    game, done = merge(game, done)
-    game = cover_up(game)[0]
+    grid_len = len(game)
+    game, done = cover_up(game, grid_len)
+    game, done = merge(game, done, grid_len)
+    game = cover_up(game, grid_len)[0]
     return game, done
 
 def right(game):
     print("right")
-    # return matrix after shifting right
+    grid_len = len(game)
     game = reverse(game)
-    game, done = cover_up(game)
-    game, done = merge(game, done)
-    game = cover_up(game)[0]
+    game, done = cover_up(game, grid_len)
+    game, done = merge(game, done, grid_len)
+    game = cover_up(game, grid_len)[0]
     game = reverse(game)
     return game, done
--- puzzle.py
+++ puzzle.py
@@ -3,7 +3,7 @@ import random
 import logic
 import constants as c
 
-def gen():
+def gen(): 
     return random.randint(0, c.GRID_LEN - 1)
 
 class GameGrid(Frame):
@@ -38,7 +38,7 @@ class GameGrid(Frame):
         self.mainloop()
 
     def init_grid(self):
-        background = Frame(self, bg=c.BACKGROUND_COLOR_GAME,width=c.SIZE, height=c.SIZE)
+        background = Frame(self, bg=c.BACKGROUND_COLOR_GAME, width=c.SIZE, height=c.SIZE)
         background.grid()
 
         for i in range(c.GRID_LEN):
@@ -73,7 +73,7 @@ class GameGrid(Frame):
             for j in range(c.GRID_LEN):
                 new_number = self.matrix[i][j]
                 if new_number == 0:
-                    self.grid_cells[i][j].configure(text="",bg=c.BACKGROUND_COLOR_CELL_EMPTY)
+                    self.grid_cells[i][j].configure(text="", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
                 else:
                     self.grid_cells[i][j].configure(
                         text=str(new_number),
@@ -82,32 +82,38 @@ class GameGrid(Frame):
                     )
         self.update_idletasks()
 
+    def can_undo(self, key):
+        return key == c.KEY_BACK and len(self.history_matrixs) > 1
+
+    def check_and_display_game_over(self):
+        state = logic.game_state(self.matrix)
+
+        if state == 'win':
+            self.grid_cells[1][1].configure(text="You", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
+            self.grid_cells[1][2].configure(text="Win!", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
+        elif state == 'lose':
+            self.grid_cells[1][1].configure(text="You", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
+            self.grid_cells[1][2].configure(text="Lose!", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
+
     def key_down(self, event):
         key = event.keysym
         print(event)
-        if key == c.KEY_QUIT: exit()
-        if key == c.KEY_BACK and len(self.history_matrixs) > 1:
+        
+        if key == c.KEY_QUIT: 
+            exit()
+        
+        if self.can_undo(key):
             self.matrix = self.history_matrixs.pop()
             self.update_grid_cells()
             print('back on step total step:', len(self.history_matrixs))
+            
         elif key in self.commands:
             self.matrix, done = self.commands[key](self.matrix)
             if done:
                 self.matrix = logic.add_two(self.matrix)
-                # record last move
                 self.history_matrixs.append(self.matrix)
                 self.update_grid_cells()
-                if logic.game_state(self.matrix) == 'win':
-                    self.grid_cells[1][1].configure(text="You", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
-                    self.grid_cells[1][2].configure(text="Win!", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
-                if logic.game_state(self.matrix) == 'lose':
-                    self.grid_cells[1][1].configure(text="You", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
-                    self.grid_cells[1][2].configure(text="Lose!", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
-
-    def generate_next(self):
-        index = (gen(), gen())
-        while self.matrix[index[0]][index[1]] != 0:
-            index = (gen(), gen())
-        self.matrix[index[0]][index[1]] = 2
+                
+                self.check_and_display_game_over()
 
 game_grid = GameGrid()
